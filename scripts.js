583eb5b67422f9a4232f96f13c7758418b8c78df
Cleyciane Farias <fariascleycianedelima@gmail.com>
2022-09-01 18:00:52 -0500

    Primeiro commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function darkMode() {
+    var element = document.body;
+     var change = document.querySelectorAll('.mudar');
+     console.log(change);
+    change.forEach((elementor)=> {
+
+        elementor.classList.toggle('test');
+    }
+    )
+
+    // var content = document.getElementById("DarkModetext");
+    element.className = "dark-mode";
+    // content.innerText = "Dark Mode is ON";
+  }
+  function lightMode() {
+    var element = document.body;
+    var content = document.getElementById("DarkModetext");
+    element.className = "light-mode";
+    content.innerText = "Dark Mode is OFF";
+  }
